a4ce7df6f3341d5e19c7c97797b6ec9aafdf8a49
zdeno-jasek <zdeno.jasek@gmail.com>
2022-10-03 20:24:16 +0200

    HighestNumber

--- src/sk/posam/javacore/market/CustomerQueue.java
+++ src/sk/posam/javacore/market/CustomerQueue.java
@@ -2,7 +2,24 @@ package sk.posam.javacore.market;
 
 /**
  * 
-
+ * V obchode je jeden rad zákazníkov, ktorý obsluhuje niekoľko pokladní. Tvojou
+ * úlohou je zistiť, ako dlho bude trvať, kým obchod obslúži všetkých
+ * zákazníkov.
+ * 
+ * vstup: customers: int[] zoznam kladných celých čísel, ktorý reprezentuje
+ * zákazníkov. Číslo označuje čas, ako dlho bude trvať obslúžiť daného
+ * zákazníka. cashDesks: int počet pokladní
+ * 
+ * výstup celé číslo, ktoré hovorí o tom, koľko času trvalo obslúžiť všetkých
+ * zákazníkov.
+ * 
+ * Príklady
+ * queueTime([5,3,4], 1) -> 12
+ * queueTime([10,2,3,3], 2) -> 12
+ * queueTime([2,3,10], 2) -> 12
+ * 
+ * Zákazníci vždy čakajú, kým sa pokladňa uvoľní. Akonáhle sa nejaká uvoľní,
+ * hneď idú k tej voľnej.
  * 
  */
 public class CustomerQueue {
