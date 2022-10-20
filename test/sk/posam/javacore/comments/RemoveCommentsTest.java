8256d5a5410a7343f681b63975ccf5a32ea10e0c
zdeno-jasek <zdeno.jasek@gmail.com>
2022-10-20 21:21:21 +0200

    many unit tests

--- test/sk/posam/javacore/comments/RemoveCommentsTest.java
+++ test/sk/posam/javacore/comments/RemoveCommentsTest.java
@@ -7,15 +7,36 @@ import org.junit.jupiter.api.Test;
 class RemoveCommentsTest {
 
 	@Test
-	void test() {
+	void doBatôžkaSiNaložPretvárkuLožAFaloš() {
 		assertEquals( "Do batôžka\n"+
 				"pretvárku\n"+
 				"\n"+
-				" a potom ich hoď $ všetky do vody.",  
+				" a potom ich hoď",  
 		RemoveComments.remove(  "Do batôžka # si nalož\n"+
 				"pretvárku $ , lož a faloš\n"+
 				"#\n"+
-				"a potom ich hoď $ všetky do vody.", new char[] { '#', '$' } ) );
+				" a potom ich hoď $ všetky do vody.", new String[] { "#", "$" } ) );
+	}
+
+	@Test
+	void tooSimpleToFail() {
+		assertEquals( "",  
+		RemoveComments.remove(  "#", new String[] { "#", "$" } ) );
+	}
+
+	@Test
+	void dontRemoveEmptyLines() {
+		assertEquals( "\n\n",  
+		RemoveComments.remove(  "  \n"+
+				"#remove this\n"+
+				"$remove that", new String[] { "#", "$" } ) );
+	}
+
+	@Test
+	void removeAnotherCommentChars() {
+		assertEquals( "Lasica\n",  
+		RemoveComments.remove(  "Lasica & Satinský\n"+
+				"xFilip", new String[] { "&", "x" } ) );
 	}
 
 }
