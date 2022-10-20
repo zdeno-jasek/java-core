0b77076b374b29fcec714081028f4eb51073ec9b
zdeno-jasek <zdeno.jasek@gmail.com>
2022-10-20 23:19:11 +0200

    Maze

---
+++ src/sk/posam/javacore/finder/Finder.java
@@ -0,0 +1,20 @@
+package sk.posam.javacore.finder;
+
+/**
+ * Vstupom je zadané bludisko štvorcového pôdorysu NxN, kde znak bodka znamená voľno a znak W znamená stena:
+ * .W.
+ * .W.
+ * ...
+ * 
+ * Úlohou metódy je vrátiť TRUE, ak je možné prejsť bludisko z ľavého horného rohu (0,0) do pravého dolného rohu (N-1,N-1).
+ * 
+ * Vstupný reťazec je zadaný ako N riadkov, čiže na konci každého riadku je "\n".
+ * Počiatočná a koncová pozícia je vždy voľná, t.j. je tam znak bodka.
+ *
+ */
+public class Finder {
+
+    public static boolean pathFinder(String maze) {
+        return false;
+    }
+}
---
+++ test/sk/posam/javacore/finder/FinderTest.java
@@ -0,0 +1,50 @@
+package sk.posam.javacore.finder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FinderTest {
+
+	@Test
+	void testWithWall() {
+
+		String a = ".W.\n" + 
+		           ".W.\n" + 
+				   "...";
+
+		assertEquals(true, Finder.pathFinder(a));
+	}
+	
+	@Test
+	void testNotSuccess() {
+
+		String b = ".W.\n" + 
+				    ".W.\n" + 
+					"W..";
+		assertEquals(false, Finder.pathFinder(b));
+	}
+	@Test
+	void biggerMaze() {
+
+		String c = "......\n" +
+				    "......\n" + 
+				    "......\n" + 
+				    "......\n" + 
+				    "......\n" + 
+				    "......";
+		assertEquals(true, Finder.pathFinder(c));
+	}
+	@Test
+	void testDeadEnd() {
+
+		String d = "......\n" + 
+				    "......\n" + 
+					"......\n" + 
+				    "......\n" + 
+					".....W\n" + 
+				    "....W.";
+
+		assertEquals(false, Finder.pathFinder(d));
+	}
+}
